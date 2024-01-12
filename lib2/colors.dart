89ec58b7eeae972809ebd94c152860037a8d950f
Mferrys <113429157+MuhammadFerrySofianshah@users.noreply.github.com>
2024-01-12 07:49:59 +0700

    Delete lib2 directory

--- lib2/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color bgColor = const Color(0xffECECEE);
-Color blackColor = const Color(0xff000000);
-Color whiteColor = const Color(0xffffffff);
-Color yellowColor = const Color(0xffFFC500);
-Color grayColor = const Color(0xffD9D9D9);
